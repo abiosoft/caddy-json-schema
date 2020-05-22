2cddcdaf97153ef35a879a2cb397e9a2db157862
Abiola Ibrahim <abiola89@gmail.com>
2020-05-22 11:25:17 +0100

    it works

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/abiosoft/caddy-json-schema
+
+go 1.14
+
+require github.com/caddyserver/caddy/v2 v2.0.0
---
+++ types.go
@@ -0,0 +1,65 @@
+package jsonschema
+
+// Module ...
+type Module struct {
+	Name string      `json:"name,omitempty"`
+	Type interface{} `json:"-"`
+	Field
+}
+
+// Modules ...
+type Modules map[string]Module
+
+var moduleMap = map[string]Modules{}
+
+// Schema ...
+type Schema struct {
+	Title       string `json:"title,omitempty"`
+	Description string `json:"description,omitempty"`
+	Type        string `json:"type,omitempty"`
+	Ref         string `json:"$ref,omitempty"`
+
+	ArrayItems           *Schema            `json:"items,omitempty"`
+	Definitions          map[string]*Schema `json:"definitions,omitempty"`
+	Properties           map[string]*Schema `json:"properties,omitempty"`
+	AdditionalProperties *Schema            `json:"additionalProperties,omitempty"`
+	Required             []string           `json:"required,omitempty"`
+
+	AdditionalItems bool      `json:"additionalItems,omitempty"`
+	AllOf           []*Schema `json:"allOf,omitempty"`
+	AnyOf           []*Schema `json:"anyOf,omitempty"`
+}
+
+func getType(typ string) string {
+	switch typ {
+	case "bool":
+		return "boolean"
+	case "int", "int8", "int16", "int32", "int64",
+		"uint", "uint8", "uint16", "uint32", "uint64":
+		return "number"
+	case "string":
+		return "string"
+	case "slice", "array":
+		return "array"
+	}
+
+	return "object"
+}
+
+func (s *Schema) setType(typ string) {
+	s.Type = getType(typ)
+}
+
+func (s *Schema) setRef(moduleID string) {
+	s.Ref = "#/definitions/" + moduleID
+}
+
+var globalSchema = NewSchema()
+
+// NewSchema ...
+func NewSchema() *Schema {
+	return &Schema{
+		Definitions: make(map[string]*Schema),
+		Properties:  make(map[string]*Schema),
+	}
+}
